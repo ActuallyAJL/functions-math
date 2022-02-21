a11191f0d55048b3e61b2c5dddbbd6af12634312
ActuallyAJL <zero3260@hotmail.com>
2022-02-21 13:11:34 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const add = (thing1 , thing2) => {
+    return thing1 + thing2;
+}
+
+const double = (thing1) => {
+    return 2 * thing1;
+}
+
+let thingMe = add(1 , 9999);
+let thingYou = double(11);
+
+console.log(thingMe);
+console.log(thingYou);
